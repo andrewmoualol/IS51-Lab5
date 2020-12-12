bda6e57be69849d5ded1076fa8848b3fed894b52
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-12 13:41:01 -0800

    finish structed english

--- Lab_5.py
+++ Lab_5.py
@@ -1,6 +1,3 @@
-
-
-
 """
 The program is trying to tanslate a sentence as user input.
 We first read in the text file textese.txt.
@@ -18,53 +15,3 @@ into our dictionary.
 After each word is translated, we then
 print out the translated sentence to the user.
 """
-
-"""
-main():
-    set sentence = input()
-    set dictionary = create_dictionary()
-    translate(sentence, dictionary)
-
-translate(sentence, dictionary):
-    words = for each of the word in the sentence,
-    for each words, translate the word
-    print translated sentence to user
-
- create_dictionary():
-    read in textese.txt
-    create list = each line from file
-    close the file
-    create a dict off of the list
-    return the dict
-        
-main()
-"""
-
-def main():
-    sentence = "enjoy the excellent band tonight"
-    dictionary = create_dictionary("textese.txt")
-    translate(sentence, dictionary)
-
-def create_dictionary(txt_file):
-    infile = open(txt_file, "r")
-    words = [word.rstrip() for word in infile]
-    infile.close()
-
-    print("words", words)
-    translation = {}
-    for word in words: # "tonight,2nite"
-        [k, v] = word.split(",") #["tonight,2nite"]
-        translation[k] = v
-    #print(translation)
-    return translation
-
-def translate(sentence, dictionary):
-    print("From translate", sentence)
-    words = sentence.split() #["enjoy", "the", "excellent", "band", "tonight",]
-    for word in words:
-        print(dictionary.get(word, word), " ", end="")
-
-main()
-
-
-
